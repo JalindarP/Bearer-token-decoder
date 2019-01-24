cbcf64a948182fc83cde2bfd73f7fc6616750bce
Jalindar Pandharkar (OWQ8) <jalindarpandharkar@gmail.com>
2019-01-24 19:57:43 +0530

    Bearer Token generator and decoder

---
+++ BearerTokenDecoder/BearerTokenDecoder/JWTDecoder.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+
+namespace BearerTokenDecoder
+{
+    public sealed class JwtDecoder
+    {
+
+        public Task<string> Decode(string tokenString)
+        {
+            if (string.IsNullOrEmpty(tokenString))
+                return Task.FromResult(string.Empty);
+
+            var token = new JwtSecurityToken(jwtEncodedString: tokenString);
+            var userId = token.Claims.First(c => c.Type == "Id").Value;
+
+            return Task.FromResult(userId);
+        }
+
+        public string Encode( string id, string firstName, string LastName, string languageIsoCode, string email, string userName)
+        {
+            var claims = new Claim[]
+            {
+                new Claim("Id", "2D355330-B65E-4DE7-8638-D64CDD28746C"),
+                new Claim("FirstName", "3B39146"),
+                new Claim("LastName", "3B39146"),
+                new Claim("LanguageIsoCode", "en"),
+                new Claim("Email", "uzache@eurofins.com"),
+                new Claim("UserName", "uzache")
+            };
+
+            var token = new JwtSecurityToken(claims: claims, audience: "http://localhost:8011/", issuer: "Online JWT Builder");
+        
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.WriteToken(token);
+            return jwt;
+        }
+    }
+}
---
+++ BearerTokenDecoder/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BearerTokenDecoder;
+using System.Configuration;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string token =  ConfigurationManager.AppSettings["token"];
+            JwtDecoder d = new JwtDecoder();
+            Console.WriteLine(d.Decode(token).Result);
+
+            string token2 = d.Encode(ConfigurationManager.AppSettings["Id"],
+                ConfigurationManager.AppSettings["FirstName"],
+                ConfigurationManager.AppSettings["LastName"],
+                ConfigurationManager.AppSettings["LanguageIsoCode"],
+                ConfigurationManager.AppSettings["Email"],
+                ConfigurationManager.AppSettings["UserName"]
+                );
+            Console.WriteLine(d.Decode(token2).Result);
+            Console.ReadLine();
+        }
+    }
+}
